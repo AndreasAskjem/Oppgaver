8da1d4afb744ced08cb40067d09c31dc1cbfd810
AndreasAskjem <andreasa@getacademy.com>
2019-09-25 21:20:07 +0200

    Labyrint
    Finished the labyrinth generator. Completely re-wrote the code because it was painfull to figure out how things worked, and trying to fix the last thing that didn't work properly.

--- Labyrintgenerator/Labyrintgenerator.js
+++ Labyrintgenerator/Labyrintgenerator.js
@@ -1,103 +1,64 @@
-//document.write('hello');
+const labyrinthSize = 10;
 
-generatelabyrinth(5);
+const room = `<td class="room"></td>`;
+const smallWall = `<td class="wall small"></td>`;
+const wideWall = `<td class="wall wide" onclick="toggleWall(this)"></td>`;
+const tallWall = `<td class="wall high" onclick="toggleWall(this)"></td>`;
+const noWideWall = `<td class="noWall wide" onclick="toggleWall(this)"></td>`;
+const noTallWall = `<td class="noWall high" onclick="toggleWall(this)"></td>`;
+
+// Puts together the rows from the other functions to make the labyrinth.
+generatelabyrinth(labyrinthSize);
 function generatelabyrinth(size){
-    let labyrinth = `<table>
-    `;
-    let labyrinthRow = ``;
+    let labyrinth = `<table>`;
+    labyrinth += solidRow(size);
+    labyrinth += roomRow(size);
 
-    for(i=0; i<size; i++){
-        labyrinthRow += `<tr>
-        `;
-        //row without rooms
-        for(j=0; j<size; j++){
-            if(i===0){
-                labyrinthRow += `<td class="wall small"></td>
-                <td class="wall wide"></td>
-                `;
-            }
-            else{
-                labyrinthRow += `<td class="wall small"></td>
-                <td class="${Math.random() < 0.5 ? 'wall' : 'noWall'} wide"></td>
-                `;
-            }
-        }
-        labyrinthRow += `<td class="wall small"></td>
-        </tr>
-        <tr>
-        `;
-
-        //row with rooms
-        for(j=0; j<=size; j++){
-            if(j===0){
-                labyrinthRow += `<td class="wall high"></td>
-                <td class="room"></td>
-                `;
-            }
-            else if(i===(size)){
-                labyrinthRow += `<td class="wall small"></td>
-                <td class="wall wide"></td>
-                `;
-            }
-            else{
-                labyrinthRow += `<td class="${Math.random() < 0.5 ? 'wall' : 'noWall'} high"></td>
-                <td class="room"></td>
-                `;
-            }
-        }
-        
-        labyrinthRow += `<td class="wall high"></td>
-        </tr>
-        `;
-        
-        labyrinth += labyrinthRow;
-        labyrinthRow = ``;
+    for(i=0; i<size-1; i++){
+        labyrinth += randomRow(size);
+        labyrinth += roomRow(size);
     }
+    labyrinth += solidRow(size);
     labyrinth += `</table>`;
-
-    //console.log(labyrinth);
     document.write(labyrinth);
-    
 }
 
 
+// Makes a solid row.
+function solidRow(size){
+    let oneRow = `<tr>`
+    for(j=0; j<size; j++){
+        oneRow += smallWall + wideWall;
+    }
+    oneRow += smallWall + `</tr>`;
+    return(oneRow);
+}
 
-x=`<table>
-    <tr>
-        <td class="wall small"></td>
-        <td class="wall wide"></td>
-        <td class="wall small"></td>
-        <td class="wall wide"></td>
-        <td class="wall small"></td>
-    </tr>
-    <tr>
-        <td class="wall high"></td>
-        <td class="room"></td>
-        <td class="wall high"></td>
-        <td class="room"></td>
-        <td class="wall high"></td>
-    </tr>
-    <tr>
-        <td class="wall small"></td>
-        <td class="wall wide"></td>
-        <td class="wall small"></td>
-        <td class="wall wide"></td>
-        <td class="wall small"></td>
-    </tr>
-    <tr>
-        <td class="wall high"></td>
-        <td class="room"></td>
-        <td class="wall high"></td>
-        <td class="room"></td>
-        <td class="wall high"></td>
-    </tr>
-    <tr>
-        <td class="wall small"></td>
-        <td class="wall wide"></td>
-        <td class="wall small"></td>
-        <td class="wall wide"></td>
-        <td class="wall small"></td>
-    </tr>
-</table>`
+// Makes a row with rooms.
+function roomRow(size){
+    let oneRow = `<tr>` + tallWall + room;
+    for(j=0; j<size-1; j++){
+        oneRow += Math.random() < 0.5 ? tallWall : noTallWall;
+        oneRow += room;
+    }
+    oneRow += tallWall + `</tr>`;
+    return(oneRow);
+}
 
-//document.write(x);
+// Makes a row without rooms.
+function randomRow(size){
+    let oneRow = `<tr>`;
+    for(j=0; j<size; j++){
+        oneRow += smallWall;
+        oneRow += Math.random() < 0.5 ? wideWall : noWideWall;
+    }
+    oneRow += smallWall + `</tr>`;
+    return(oneRow);
+}
+
+
+// Toggles wide/tall walls between shown and not shown when clicked.
+function toggleWall(wall){
+    wall.classList.toggle('noWall');
+    wall.classList.toggle('wall');
+}
