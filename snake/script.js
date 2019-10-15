2641bd9f02157a5ea231ebe3bbd6d75a9ef56b80
Andreas Askjem <andaskjem@gmail.com>
2019-10-15 09:18:34 +0200

    New beginning

--- snake/script.js
+++ snake/script.js
@@ -1,81 +1,81 @@
-
-let boardModel;
-let boardView = document.getElementById('board');
-let size = {
-    width: 2,
-    height: 2
-};
-
-//init(size);
-//showBoard();
-
-
-init();
-setInterval(placeApple, 500);
-function init(){
-    boardModel = {};
-    boardModel.rows = [];
-
-    for(rowIndex=0; rowIndex<size.height; rowIndex++){
-        let newRow = {};
-        newRow.cells = [];
-        for(newCell=0; newCell<size.width; newCell++){
-            let newCell = {};
-            newCell.Head = false;
-            newCell.Bodey = false;
-            newCell.Apple = false;
-            newRow.cells.push(newCell);
+var snakeModel;
+initModel();
+updateView();
+//////////////////////setInterval(move, 200);
+function updateView() {
+    var snakeTable = document.getElementById('snakeTable');
+    snakeTable.innerHTML = '';
+    for (var rowIndex = 0; rowIndex < snakeModel.size; rowIndex++) {
+        var tr = snakeTable.insertRow();
+        for (var columnIndex = 0; columnIndex < snakeModel.size; columnIndex++) {
+            var td = tr.insertCell();
         }
-        boardModel.rows.push(newRow);
     }
-    placeSnake();
-    placeApple();
-    showBoard();
-}
-
-function placeSnake(){
-    heightPlacement = Math.floor(size.height/2);
-    //boardModel.rows[heightPlacement].cells[1].Head = true;
-}
-
-function placeApple(){
-    let position = randomPosition();
-    //let currentCell = boardModel.rows[position.y].cells[position.x];
-    /*while(currentCell.Head || currentCell.Body){
-        console.log('hello');
-        position = randomPosition();
-        currentCell = boardModel.rows[position.y].cells[position.x];
-    }*/
-    
-    boardModel.rows[position.y].cells[position.x].Apple = true;
-    showBoard();
-}
-
-function randomPosition(){
-    let x = Math.floor(Math.random()*size.width);
-    let y = Math.floor(Math.random()*size.height);
-    position = {
-        x: x,
-        y: y
+    var bodyPart = snakeModel.snakeHead;
+    while (bodyPart != null) {
+        var td = snakeTable.rows[bodyPart.y].cells[bodyPart.x];
+        td.classList.add('snake');
+        bodyPart = bodyPart.nextBodyPart;
     }
-    return(position);
+    var food = snakeModel.food;
+    var td = snakeTable.rows[food.y].cells[food.x];
+    td.classList.add('apple');
 }
 
-// Creates the shown HTML, with the correct values inserted.
-function showBoard(){
-    boardView.innerHTML = '';
-    for(rowIndex=0; rowIndex<size.height; rowIndex++){
-        let viewRow = boardView.insertRow();
-        let modelRow = boardModel.rows[rowIndex];
-        for(cellIndex=0; cellIndex<size.width; cellIndex++){
-            let viewCell = viewRow.insertCell();
-            let modelCell = modelRow.cells[cellIndex];
-            if(modelCell.Head){
-                viewCell.style.backgroundColor = 'black';
-            }
-            else if(modelCell.Apple){
-                viewCell.style.backgroundColor = 'red';
-            }
+function initModel() {
+    snakeModel = { size: 10 };
+    snakeModel.snakeHead = {x: 2, y:Math.floor(snakeModel.size/2)};
+    snakeModel.food = createRandomPosition();
+    snakeModel.growCount = 3;
+    snakeModel.nextBodyPart = null;
+    snakeModel.direction = { x: 1, y: 0 }; ////
+}
+function createRandomPosition() {
+    return {
+        x: Math.floor(Math.random() * snakeModel.size),
+        y: Math.floor(Math.random() * snakeModel.size)
+    }
+}
+function createNewSnakeHead() {
+    return {
+        x: snakeModel.snakeHead.x + snakeModel.direction.x,
+        y: snakeModel.snakeHead.y + snakeModel.direction.y,
+        nextBodyPart: snakeModel.snakeHead
+    };
+}
+function move() {
+    if (!snakeModel.direction) return;
+    snakeModel.snakeHead = createNewSnakeHead();
+    if (snakeModel.growCount > 0) {
+        snakeModel.growCount--;
+    } else {
+        // Slette siste element
+        var bodyPart = snakeModel.snakeHead;
+        var lastBodyPart = null;
+        while (bodyPart.nextBodyPart != null) {
+            lastBodyPart = bodyPart;
+            bodyPart = bodyPart.nextBodyPart;
         }
+        if (lastBodyPart !== null) {
+            lastBodyPart.nextBodyPart = null;
+        }
+    }
+    if (snakeModel.snakeHead.x === snakeModel.food.x
+        && snakeModel.snakeHead.y === snakeModel.food.y) {
+        snakeModel.food = createRandomPosition();
+        snakeModel.growCount = 1;
+    }
+    updateView();
+}
+function controlSnake(e) {
+    d = snakeModel.direction;
+    if (e.keyCode == 37 && d.x != 1) { // left
+        snakeModel.direction = { x: -1, y: 0 };
+    } else if (e.keyCode == 39 && d.x != -1) { // right
+        snakeModel.direction = { x: 1, y: 0 };
+    } else if (e.keyCode == 38 && d.y != 1) { // up
+        snakeModel.direction = { x: 0, y: -1 };
+    } else if (e.keyCode == 40 && d.y != -1) { // down
+        snakeModel.direction = { x: 0, y: 1 };
     }
 }
